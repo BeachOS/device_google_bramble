5c9e4de222cdeec8e5551d7f804f522f82efac7a
root <Beach OS>
2023-01-11 08:07:50 +0000

    BeachOS

---
+++ beach_bramble.mk
@@ -0,0 +1,22 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/beach/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/bramble/device-beach.mk)
+$(call inherit-product, device/google/redbull/device-beach.mk)
+$(call inherit-product, device/google/bramble/aosp_bramble.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := beach_bramble
+PRODUCT_MODEL := Pixel 4a (5G)
+PRODUCT_BRAND := google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=bramble \
+    PRIVATE_BUILD_DESC="bramble-user 13 TQ1A.230105.001 9292298 release-keys"
+
+BUILD_FINGERPRINT := google/bramble/bramble:13/TQ1A.230105.001/9292298:user/release-keys
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+$(call inherit-product, vendor/google/bramble/bramble-vendor.mk)
